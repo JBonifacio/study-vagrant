7ddf649286f077478093ba85600d1166ea4210b6
Joseph Bonifacio <bonifacio.joseph@gmail.com>
2016-08-08 17:26:47 -0700

    Vagrant updates.

--- scripts/batchInstall.sh
+++ scripts/batchInstall.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
+
+echo "Update package lists..."
 sudo apt-get -y update
 
 # Install NGINX
+echo "Install NGINX..."
 sudo apt-get -y install nginx
 
 # Install PHP7
+echo "Install PHP and dependencies..."
 sudo apt-get install python-software-properties
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get -y install php7.0 php7.0-fpm
 
 # Install PostgreSQL
+echo "Install Postgres and PHP-Postgres Support..."
 sudo apt-get -y install postgresql postgresql-contrib
 
 # Install PSQL Support for PHP
 sudo apt-get -y install php7.0-pgsql
 
 # Install vim
+echo "Install vim..."
 sudo apt-get -y install vim
 
 # Install Git
@@ -50,11 +56,21 @@ sed -i -e 's/# Example aliases/source ~\/.bash_aliases/gi' ~vagrant/.zshrc
 chsh -s /bin/zsh vagrant
 
 ##
+# Configure Postgres
+##
+
+# Create role
+sudo su postgres -c "psql -c \"CREATE ROLE vagrant SUPERUSER LOGIN PASSWORD 'vagrant'\" "
+
+# Create db
+sudo su postgres -c "createdb -O vagrant vagrant"
+
+## 
 #  Configure Webserver
 ##
 
 # Link synced folder with code to NGINX default.
-ln -sfn /home/vagrant/www /usr/share/nginx/html
+ln -sfn /home/vagrant/code /usr/share/nginx/html
 
 # Delete default file
 rm -rf /etc/nginx/sites-available/default
